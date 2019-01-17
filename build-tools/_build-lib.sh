9a1dde39e3a308c7cb52d438c619b7a015def2fe
basavarg <basavarg@in.ibm.com>
2019-01-17 13:26:35 -0600

    sleep 8m

--- build-tools/_build-lib.sh
+++ build-tools/_build-lib.sh
@@ -150,7 +150,7 @@ tmpdir_for_test() {
   # Copy over mounted src to our writable src
   rsync -a --exclude '.git' --exclude '_docker_workspace' $GOPATH/src/ $WKDIR/src
   else #rsync takes few mins to sync on ppc64le so adding sleep
-   while sleep 10m; do echo "=====[ $SECONDS seconds, rsync still in progress.. ]====="; done &
+   while sleep 8m; do echo "=====[ $SECONDS seconds, rsync still in progress.. ]====="; done &
    rsync -a --exclude '.git' --exclude '_docker_workspace' $GOPATH/src/ $WKDIR/src
   fi
   echo $WKDIR
